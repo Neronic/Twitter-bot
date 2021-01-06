3b2bf0028832679ce4da49b11b91b2e6bcf0cda2
Jacob Fields <sjacobfields@gmail.com>
2021-01-06 13:39:52 -0500

    Finished DatePost
    Finished the datepost script. It now posts daily on the given hour. The post gets formated into weekday, monday, day, and year. It also checks if it's wednesday to add an additional message.

--- Twitter_bot/Twitter_bot/bots/datepost.py
+++ Twitter_bot/Twitter_bot/bots/datepost.py
@@ -4,19 +4,25 @@ from config import create_api
 import time
 import datetime
 
-logging.basiscConfig(_levelToName=logging.INFO)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
-TIME_TO_TWEET = 8
+TIME_TO_TWEET = 13
 
 last_tweeted = datetime.datetime.now().replace(year=datetime.MINYEAR)
+wednesday_tweet = "It's Wednesday my dudes"
 
 def tweet_daily(api):
     today_date = datetime.datetime.now()
 
     if (not last_tweeted.day == today_date.day or last_tweeted.year == datetime.MINYEAR) and today_date.hour == TIME_TO_TWEET:
-        api.update_status("The date is: " + today_date().strftime('%m/%d/%Y'))
-        logger.info(f"Tweeted {text} at {datetime.now().strftime('%m/%d/%Y at %H:%M:$S')}")
+        if (today_date.weekday() == 2):
+            api.update_status("The date is: " + today_date.strftime('%A, %B the %d, %Y') + "\nIt's Wednesday my dudes")
+            logger.info(f"Tweeted at {datetime.datetime.now().strftime('%m/%d/%Y at %H:%M:%S')}")
+        else:
+            api.update_status("The date is: " + today_date.strftime('%A, %B the %d, %Y'))
+            logger.info(f"Tweeted at {datetime.datetime.now().strftime('%m/%d/%Y at %H:%M:%S')}")
+
         return today_date
     else:
         logger.info(f"Already tweeted or not time to tweet.")
@@ -24,7 +30,6 @@ def tweet_daily(api):
 
 def main():
     api = create_api()
-    i = 0
     while True:
         last_tweeted = tweet_daily(api)
         logger.info("Waiting...")
